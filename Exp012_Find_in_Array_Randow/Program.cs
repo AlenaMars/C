d0e3a617418f9a159191e3224b8e45d01f83aced
Alena <alenap89@mail.ru>
2022-10-05 15:57:57 +1000

    Лекция 2. Работа с массивом. Функция заполнения массива рандомными числами и находит элемент массива

---
+++ Exp012_Find_in_Array_Randow/Program.cs
@@ -0,0 +1,49 @@
+﻿// 
+void FillArray(int[] collection) 
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length) 
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PtintArray(int[] col) 
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count) 
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array = new int[10];
+
+FillArray(array); // Заполнил массив
+PtintArray(array); // Расспечатал массив
+
+Console.WriteLine();
+
+int pos = IndexOf(array, 466);
+Console.WriteLine(pos);
