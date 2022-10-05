b9c49d3df7307d5c06e6f01c6b515b5a86bfcff2
Alena <alenap89@mail.ru>
2022-10-05 14:30:41 +1000

    Лекция 2. Работа с массивом. Поиск максимума из 9 значений

--- Exp009_Array/Program.cs
+++ Exp009_Array/Program.cs
@@ -1,4 +1,5 @@
-﻿int Max(int arg1, int arg2,int arg3)
+﻿// Урок 2. Лекция. 5.10.2022. Введение в Функции
+int Max(int arg1, int arg2,int arg3)
 {
     int result = arg1;
     if(arg2 > result) result = arg2;
@@ -10,15 +11,18 @@ int a1 = 15;
 int b1 = 21;
 int c1 = 4;
 int a2 = 315;
-int b2 = 251;
+int b2 = 2451;
 int c2 = 45;
 int a3 = 145;
 int b3 = 231;
 int c3 = 34;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
-
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3)); //Переместили переменные в функцию
 Console.WriteLine(max);
+
+
+
---
+++ Exp010_IntroArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// Урок 2. Лекция. 05.10.2022 Массивы
+int Max(int arg1, int arg2,int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+//                0   1   2   3   4   5    6   7  8
+int [] array = {133, 22, 14, 55, 45, 127, 66, 66, 5};
+array[4] = 12; //Заменяет 45 - под индексом 4 на 12
+Console.WriteLine(array[4]);
+
+
+int max = Max(Max(array[0], array[1], array[2]), 
+              Max(array[3], array[4], array[5]), 
+              Max(array[6], array[7], array[8])); // Поиск макс из 9
+
+Console.WriteLine();
+Console.WriteLine(max);
