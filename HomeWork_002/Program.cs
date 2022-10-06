1272fd4af084bdfd00eeda939b91b1083c486733
Alena <alenap89@mail.ru>
2022-10-06 21:57:43 +1000

    Добавила 1 задачу из домашней работы. Выделить второй знак из трехзначного числа

---
+++ HomeWork_002/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+Задача 10: Напишите программу, 
+которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+*/
+
+int a = new Random().Next(100,999);
+int b = int.Parse(int.Parse(a.ToString().Remove(0, 1)).ToString().Remove(1, 1));
+Console.WriteLine($"{a} -> {b}");
+
