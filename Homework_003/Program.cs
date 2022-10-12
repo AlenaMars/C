02d0ad8704d43fb1a3fe98548e166bd2ec7057b7
Alena <alenap89@mail.ru>
2022-10-13 09:21:13 +1000

    Домашняя работа 3 задача 1. Зеркальное число

---
+++ Homework_003/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+/*
+    Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+*/
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string a = Console.ReadLine()!;
+int length = a.Length;
+    for (int i = 0; i < length/2; i++)
+    {
+        if(a[i] != a[length-1-i])
+        {
+            Console.Write("Не палиндром");
+        }  
+       else Console.Write("Палиндром");
+       break;
+    }
+    
+
+
