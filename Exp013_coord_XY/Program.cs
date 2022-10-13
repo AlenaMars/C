047325fb799768a6d4e39ac217c529a8e950e5f9
Alena <alenap89@mail.ru>
2022-10-13 12:20:00 +1000

    Семинар 3. Найти четверть по координатам-другие варианты решения

--- Exp013_coord_XY/Program.cs
+++ Exp013_coord_XY/Program.cs
@@ -11,3 +11,45 @@ if (x > 0 && y > 0) Console.Write($"1 четверти");
 if (x < 0 && y > 0) Console.Write($"2 четверти");
 if (x < 0 && y < 0) Console.Write($"3 четверти");
 if (x > 0 && y < 0) Console.Write($"4 четверти");
+
+
+/*//Можем ввести проверку на корректно введеный код
+//например
+//дополним
+
+int x, y;
+Console.Write($"Введите координату X: ");
+bool flag1 = int.TryParse(Console.ReadLine()!, out x); // берем строку переводим в число выводим в out
+Console.Write($"Введите координату Y: ");
+bool flag2 = int.TryParse(Console.ReadLine()!, out y);
+if(!flag1 || !flag2) Console.Write($"Неверный код");
+else
+{
+    if (x > 0 && y > 0) Console.Write($"1 четверти");
+    if (x < 0 && y > 0) Console.Write($"2 четверти");
+    if (x < 0 && y < 0) Console.Write($"3 четверти");
+    if (x > 0 && y < 0) Console.Write($"4 четверти");
+}
+
+//bool хранит true false
+// можно переписать задачу на 2 проверки
+
+int x, y;
+Console.Write($"Введите координату X: ");
+int.TryParse(Console.ReadLine()!, out x); // берем строку переводим в число выводим в out
+Console.Write($"Введите координату Y: ");
+int.TryParse(Console.ReadLine()!, out y);
+
+if (x > 0)
+{
+    if(y > 0) Console.Write($"1 четверти");
+    else
+    Console.Write($"4 четверти");
+}
+else
+{
+    if(y > 0) Console.Write($"2 четверти");
+    else
+    Console.Write($"3 четверти"); 
+}
+*/
