4d13df29e8b0bf0f49b48cd2ae8606533682d914
Alena <alenap89@mail.ru>
2022-10-13 12:18:07 +1000

    Семинар 3. Найти четверть по координатам

---
+++ Exp013_coord_XY/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 17. Напишите программу, которая принимает на вход координаты точки (X и Y), причем х у не равны 0 и выдает номер четверти плоскости, 
+в которой находится эта точка
+*/
+
+int x, y;
+Console.Write($"Введите координату X: ");
+int.TryParse(Console.ReadLine()!, out x); // берем строку переводим в число выводим в out
+Console.Write($"Введите координату Y: ");
+int.TryParse(Console.ReadLine()!, out y);
+if (x > 0 && y > 0) Console.Write($"1 четверти");
+if (x < 0 && y > 0) Console.Write($"2 четверти");
+if (x < 0 && y < 0) Console.Write($"3 четверти");
+if (x > 0 && y < 0) Console.Write($"4 четверти");
