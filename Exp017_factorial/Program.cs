6a182396544b2f8fe4bfc4eea58173eb6ae5a4d8
Alena <alenap89@mail.ru>
2022-10-13 14:41:54 +1000

    Семинар 4. Функция вычисления факториала

---
+++ Exp017_factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿/* **Задача 28:**
+Напишите программу, которая принимает на вход число N и
+выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120 */
+
+int a;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+Console.Write($"Факториал числа {a} = {Factorial(a)}");
+
+int Factorial(int a)
+{
+int f = 1;
+for (int i = 2; i <= a; i++)
+f = f * i;
+return f;
+}
