e4962b3ee9d4aab2c5e13721852763b05a85b942
Alena <alenap89@mail.ru>
2022-10-13 10:10:39 +1000

    Домашняя работа 3 задача 3. Вывести таблицу кубов

---
+++ Homework_003_03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.Clear();
+Console.Write($"Введите число N: ");
+int.TryParse(Console.ReadLine()!, out int N);
+void Cube(int num) {
+    Console.Write($"Таблица кубов: ");
+    for(int i = 1; i <= num; i++) {
+        int c = (int)Math.Pow(i,3);
+        Console.Write($"{c} ");
+    }
+}
+Cube(N);
