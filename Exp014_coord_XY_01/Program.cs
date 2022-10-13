2d3ab2c71462949513ee3008a4bd880e31e7b7c2
Alena <alenap89@mail.ru>
2022-10-13 12:23:23 +1000

    Семинар 3. По номеру четверти показывает возможные значения XY

---
+++ Exp014_coord_XY_01/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 18. Напишите программу, которая принимает на вход четверть и показывает диапозон возможных координат точек в этой четверти (x и y)
+*/
+int x;
+Console.Write($"Введите номер четверти: ");
+int.TryParse(Console.ReadLine()!, out x); // берем строку переводим в число выводим в out
+switch (x)
+{
+    case 1: //case 5:
+        Console.Write($"x > 0 y > 0");
+        break;
+    case 2:
+        Console.Write($"x < 0 y > 0");
+        break;
+    case 3:
+        Console.Write($"x < 0 y < 0");
+        break;
+    case 4:
+        Console.Write($"x > 0 y < 0");
+        break;
+    default:
+        Console.Write($"Не верный ввод"); // во всех остальных случаях
+        break;
+}
