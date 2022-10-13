02b444310b58fc3072e38d1d7921b92e2c0e10dc
Alena <alenap89@mail.ru>
2022-10-13 12:30:33 +1000

    Семинар 3. Находит отрезок между двумя точками

---
+++ Exp014_coord_XY_02/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 20. Напишите программу которая принимает на вход координаты 2 точек
+и находит расстояние между ними в 2d 
+*/
+
+int[] A = new int[2] {3, 6};
+int[] B = new int[2] {7, 1};
+
+
+int x = (int)Math.Pow(A[0]-B[0], 2);
+int y = (int)Math.Pow(A[1]-B[1], 2);
+double ans = Math.Sqrt(x+y);
+ans  = Math.Round(ans, 2);
+Console.Write($"{ans}");
+
