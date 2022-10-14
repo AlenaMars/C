2cca5add4076b129ea7d9808122bf0c898f30da6
Alena <alenap89@mail.ru>
2022-10-14 10:28:31 +1000

    Задаёт массив из 8 элементов и выводит их на экран

---
+++ Homework_004_03/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] array = CreateArray();
+foreach (var item in array)
+Console.Write($"{item} ");
+Console.Write($"]");
+
+int[] CreateArray()
+{
+Console.Write($"[ ");
+int[] _array = new int[8];
+for (int i = 0; i < _array.Length; i++)
+_array[i] = new Random().Next(0, 100);
+return _array;
+}
