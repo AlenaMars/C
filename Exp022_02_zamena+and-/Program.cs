9cab373b0a19d6bc1ffac26502452e6f9322b388
Alena <alenap89@mail.ru>
2022-10-18 16:16:24 +1000

    Семинар 5. Замена положительныхна отрицательные в массиве и наоборот.

---
+++ Exp022_02_zamena+and-/Program.cs
@@ -0,0 +1,21 @@
+﻿/* **Задача 32:** Напишите программу замена элементов массива: положительные элементы замените на соответствующие
+отрицательные, и наоборот.
+
+[-4, -8, 8, 2] -> [4, 8, -8, -2] */
+
+const int length = 12;
+int[] array = new int[length];
+
+for (int i = 0; i < length; i++)
+{
+array[i] = new Random().Next(-9, 10);
+Console.Write($"{array[i]} ");
+}
+
+for (int i = 0; i < length; i++)
+array[i] *= -1;
+
+Console.WriteLine();
+
+for (int i = 0; i < length; i++)
+Console.Write($"{array[i]} ");
