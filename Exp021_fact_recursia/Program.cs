4b083e7531dc59b38c1f63bfb202a3e05a53dd41
Alena <alenap89@mail.ru>
2022-10-18 15:55:14 +1000

    Лекция. Рекурсия факториал и фибоначчи

---
+++ Exp021_fact_recursia/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// //рекурсия это функция вызывающая саму себя
+
+// double Factorial(int n)
+// {
+//     if(n == 1) return 1;
+//     else return n * Factorial(n-1);
+// }
+
+// Console.WriteLine(Factorial(5)); //1* 2 * 3 // будет переполнение типа поэтому меняем на double
+
+double Fibonacci(int n)
+{
+ if(n == 1 || n == 2) return 1;
+ else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 50; i++)
+{
+ Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
