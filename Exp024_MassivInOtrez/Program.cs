2ebfac3f2c123b0eb2d07239da08419080f163cd
Alena <alenap89@mail.ru>
2022-10-19 16:22:06 +1000

    Семинар 5. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].

---
+++ Exp024_MassivInOtrez/Program.cs
@@ -0,0 +1,24 @@
+﻿/* **Задача 35:** Задайте одномерный массив из 123 случайных чисел.
+Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5 */
+
+const int length = 123;
+int[] array = new int[length];
+
+for (int i = 0; i < length; i++)
+{
+array[i] = new Random().Next(150);
+Console.Write($"{array[i]} ");
+}
+
+int count = 0;
+
+foreach (var item in array)
+{
+if (item >= 10 && item <= 99)
+count++;
+}
+
+Console.Write($"\nAnswer = {count} ");
