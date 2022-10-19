072753f0cda9077e7049ead0fc3cbffe5cfc09d7
Alena <alenap89@mail.ru>
2022-10-19 10:00:34 +1000

    ДЗ 5-1. Количество четных чисел в массиве.

---
+++ Homework_005_01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+const int size = 4;
+int[] array = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+array[i] = new Random().Next(100, 999);
+Console.Write($"{array[i]} ");
+}
+
+int N = 0;
+for (int i = 0; i < size; i++)
+{
+    if (array[i]%2 == 0)
+    N += 1;
+}
+Console.WriteLine();
+Console.WriteLine($"Количество чётных чисел в массиве -> {N}");
