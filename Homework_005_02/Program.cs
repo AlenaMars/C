b65f00eade3f23403b1a15ceaf2d5036bc571b9c
Alena <alenap89@mail.ru>
2022-10-19 10:38:08 +1000

    ДЗ 5-2. Сумма элементов стоящих на нечетных позициях

---
+++ Homework_005_02/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+const int size = 4;
+int[] array = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+array[i] = new Random().Next(-20, 20);
+Console.Write($"{array[i]} ");
+}
+
+int sum = 0;
+for (int i = 0; i < size; i++)
+{
+    if ( i % 2 != 0 )
+sum += array[i];
+}
+
+
+Console.Write($"\nСумма элементов = {sum}");
