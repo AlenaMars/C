2810c4bcbe49003a88f2416292f7ead80a7fca46
Alena <alenap89@mail.ru>
2022-10-27 17:24:32 +1000

    Семинар 7. Двумерные массивы. Произведение м и н

---
+++ Exp029_2marray01/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задать двумерный массив m n, каждый элемент которого будет произведением m n
+
+int[,] array = new int[3, 4];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength; j++)
+    {
+        array[i, j] = i + j;
+        Console.Write($"{array[i, j]}");
+    }
+    Console.WriteLine();
+}
