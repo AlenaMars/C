642632bfcb3a49e6176d931b6e836d4d675745d1
Alena <alenap89@mail.ru>
2022-10-27 20:26:24 +1000

    Семинар 7. Задайте 2 мерный массив найдите сумму главной диагонали

---
+++ Exp029_2marray03/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте 2 мерный массив найдите сумму главной диагонали
+
+int[,] array = new int[3, 4];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+int sum = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (i == j)
+        {
+            sum += array[i, j];
+        }   
+    }
+}
+Console.Write(sum);
